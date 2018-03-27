d6efc21a63b51c50a48820bb53c2278e8a5a075e
Kaiwen Liu <33501388+kaiwenliu@users.noreply.github.com>
2018-03-26 17:05:02 -0700

    📜 bug fixes and comments added

--- searchingdiscordbot.py
+++ searchingdiscordbot.py
@@ -18,17 +18,25 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.lower().startswith("?ping"):
+        #?ping
         print(info + message.content + " was executed by " + str(message.author))
         await client.send_message(message.channel, "<@%s> ping pong masterrace!!!" % message.author.id)
     if message.content.lower().startswith("?google"):
-        print(info + "?ping was executed by " + str(message.author))
+        #?google
+        print(info + message.content + " was executed by " + str(message.author))
+        #split the search terms into a list
         searchtermlist = str(message.content).split(" ")
+        #make sure they specify the amount of results they want
         try:
+            #the amount of results
             resultamount = int(searchtermlist[1])
+            #onlt 5 or less results to prevent spam
             if resultamount > 5:
                 await client.send_message(message.channel, "<@%s> you can only get 5 or less results in order to prevent spam." % message.author.id)
                 resultamount = 0
-            searchterms = " ".join(searchtermlist[1:])
+            #search terms rejoined
+            searchterms = " ".join(searchtermlist[2:])
+            #print all the urls searched
             for url in search(searchterms, num=resultamount, stop=1, pause=2):
                 await client.send_message(message.channel, url)
         except ValueError:
